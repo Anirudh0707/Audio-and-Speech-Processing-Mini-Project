1f49872201b7b6d195c1d77fd6e4133f722d0896
Anirudh0707 <yani.bh@gmail.com>
2018-11-03 14:14:53 +0530

    Add files via upload

--- DTW.m
+++ DTW.m
@@ -1,13 +1,13 @@
-function dtw_dist = DTW(lpc_audio, lpc_template)
-col_audio = size(lpc_audio,2);
-col_template = size(lpc_template,2);
+function dtw_dist = DTW(audio, template)
+col_audio = size(audio,2);
+col_template = size(template,2);
 
 D=zeros(col_audio+1,col_template+1)+Inf; % cache matrix
 D(1,1)=0;
 
 for i = 1:col_audio
     for j = 1:col_template 
-        cost=Euclid(lpc_audio(:,i),lpc_template(:,j));
+        cost=Dist(audio(:,i),template(:,j));
         D(i+1,j+1)=cost+min( [D(i,j+1), D(i+1,j), D(i,j)] );
         
     end
